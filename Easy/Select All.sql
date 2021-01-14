d23012da718c9a78a45536d344e4268cc737283f
Shivi_Bhatt <in.shivi@gmail.com>
2021-01-14 01:18:23 -0800

    Create Select All.sql

---
+++ Easy/Select All.sql
@@ -0,0 +1,15 @@
+/*The CITY table is described as follows:*/
+/*
+TABLE : CITY 
+FIELD.      | TYPE
+ID          | NUMBER
+NAME        | VARCHAR2(30)
+COUNTRYCODE |VARCHAR(3)
+DISTRICT    |VARCHAR(20)
+POPULATION  | NUMBER
+
+*/
+
+
+select * from city
+
