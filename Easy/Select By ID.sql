e3f461b5b172600ab3ba881f14ada39979af8eed
Shivi_Bhatt <in.shivi@gmail.com>
2021-01-14 01:20:28 -0800

    Create Select By ID.sql

---
+++ Easy/Select By ID.sql
@@ -0,0 +1,18 @@
+--Query all columns for a city in CITY with the ID 1661.
+/*The CITY table is described as follows:
+
+TABLE : CITY 
+FIELD.      | TYPE
+ID          | NUMBER
+NAME        | VARCHAR2(30)
+COUNTRYCODE |VARCHAR(3)
+DISTRICT    |VARCHAR(20)
+POPULATION  | NUMBER
+*/
+
+
+--QUERY 
+SELECT * FROM CITY 
+WHERE ID = 1661
+
+
