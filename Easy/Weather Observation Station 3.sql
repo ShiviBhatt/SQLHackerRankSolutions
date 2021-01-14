0238fb7b81a1d483f9688cdf3ecae22a6ac4b4b8
Shivi_Bhatt <in.shivi@gmail.com>
2021-01-14 01:32:55 -0800

    Create Weather Observation Station 3.sql

---
+++ Easy/Weather Observation Station 3.sql
@@ -0,0 +1,18 @@
+/*Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+The STATION table is described as follows: */
+/*
+TABLE : STATION 
+FIELD       | TYPE
+--------------------
+ID          | NUMBER
+CITY        | VARCHAR2(30)
+STATE       | VARCHAR(2)
+LAT_N       | NUMBER
+LONG_W      | NUMBER
+*/
+
+-- QUERY 
+
+SELECT DISTINCT CITY FROM STATION WHERE (ID % 2) = 0
+
+
