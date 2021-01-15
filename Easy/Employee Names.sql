90c7a540c3920dcf57ee748e714f32df8cccea21
Shivi_Bhatt <in.shivi@gmail.com>
2021-01-14 18:10:22 -0800

    Create Employee Names.sql

---
+++ Easy/Employee Names.sql
@@ -0,0 +1,13 @@
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+Input Format
+
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+
+--QUERY
+
+SELECT NAME FROM EMPLOYEE
+ORDER BY NAME
+
+
